bf8ad4650ba7139407875352bba34a92786ae5f7
ALI <alihak3er@daddy.com>
2023-04-28 18:49:24 +0500

    initial

---
+++ src/Person/Footer.jsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>
+            hi. i am ALI HASSAN
+        </h1>
+    </div>
+  )
+}
+export default Footer;
---
+++ src/Person/Header.jsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+
+const Header = () => {
+    return (
+        <div>
+        <h1>This is Header Component</h1>
+        <Footer/>
+        </div>
+    );
+};
+
+export default Header;
